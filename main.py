04f6b0980beddf03fd8a72e1f709b0ac9e0a7432
thomascowan <67574099+thomascowan@users.noreply.github.com>
2022-03-08 22:40:07 +1100

    Add more testing states, started developing way to add radius to points
    Added a state to allow fast loading which only loads data from 1st of July 2020. I have also created another file to test converting a point in a matrix to having a radius. I also changed the method of showing the data from a heatmap to a 2D histogram as it is is more useful for what I am trying to do

--- main.py
+++ main.py
@@ -19,10 +19,14 @@ MW = {}
 MVA = {}
 LocationsDF = None
 __name__ = "__main__"
-subsetData = 1
 # 0 for MW, 1 for MVA, anything else for both
 SelectedData = 0
-DEBUG = 1
+# Only loads 5 suburbs
+subsetData = 0
+#Print debug statements
+DEBUG = 0
+#only load data from 1 July 2020
+quickLoad = 1
             
 def loadAllData(directory):
     timeStarted = datetime.now() if DEBUG else ""
@@ -34,7 +38,7 @@ def loadAllData(directory):
             for row in df[filename].iterrows():
                 if filename not in MW:
                     MW[filename] = {}
-                if(subsetData and row[1]['Date'] == '15-Jul-20'):
+                if(quickLoad and row[1]['Date'] == '02-Jul-20'):
                    break
                 if row[1]['Date'] not in MW[filename]:
                     MW[filename][row[1]['Date']] = {}
@@ -59,7 +63,7 @@ def loadMWData(directory):
             for (idx, row) in enumerate(df[filename].iterrows()):
                 if filename not in MW:
                      MW[filename] = {}
-                if(subsetData and row[1]['Date'] == '02-Jul-20'):
+                if(quickLoad and row[1]['Date'] == '02-Jul-20'):
                    break
                 if row[1]['Date'] not in MW[filename]:
                      MW[filename][row[1]['Date']] = {}
@@ -80,7 +84,7 @@ def loadMVAData(directory):
             for row in df[filename].iterrows():
                 if filename not in MVA:
                     MVA[filename] = {}
-                if subsetData and row[1]['Date'] == '05-Jul-20':
+                if quickLoad and row[1]['Date'] == '02-Jul-20':
                     break
                 if row[1]['Date'] not in MVA[filename]:
                     MVA[filename][row[1]['Date']] = {}
@@ -90,50 +94,46 @@ def loadMVAData(directory):
     print(" - Time Elapsed: " + str(timeEnded - timeStarted)) if DEBUG else ""
     print("length of dictionary is " + str(len(MVA))) if DEBUG else ""
             
-#def getLocations(directory):
-##    print(locationsDF)
-
 def LoadCoordsOntoMap(directory):
     locationsDF = pd.read_csv(os.path.join(directory,"locations.csv"))
     locationsDF = locationsDF[locationsDF["Longitude"] != 0.0]
-    BBox = ((locationsDF['Longitude'].min(), locationsDF['Longitude'].max()),(locationsDF['Latitude'].min(),locationsDF['Latitude'].max()))    
-#    print(MW["Acacia Ridge_EGX_20202021.csv"])
-    scatterPlots = [list(),list(),list()]
+    
+    scatterPlots = [list(),list(),list(),list()]
     for row in locationsDF.iterrows():
         scatterPlots[0].append(row[1]["Longitude"])
         scatterPlots[1].append(row[1]["Latitude"])
         scatterPlots[2].append(row[1]["Location"])
+        scatterPlots[3].append(MW[row[1]["Location"]]["01-Jul-20"]["12:00:00 AM"]*1000)
         
-    plt.figure(figsize = (14, 7))
-    fig, ax = plt.subplots()
-    for i in range(0,len(scatterPlots[0])):
-#        print("aaaa:  " + str(scatterPlots[0][i]) + " - " + str(scatterPlots[1][i]) + " - " + str(scatterPlots[2][i]))
-        ax.scatter(scatterPlots[0][i],scatterPlots[1][i],s=MW[scatterPlots[2][i]]["01-Jul-20"]["12:00:00 AM"])
-        
+    heatmap, xedges, yedges = np.histogram2d(scatterPlots[0], scatterPlots[1], weights=scatterPlots[3], density=True, bins=500)
+    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+    
+    plt.clf()
+    plt.figure(figsize = (7, 14))
+    plt.imshow(heatmap.T, extent=extent, origin='lower')
     plt.show()
-#    plt.scatter(scatterPlots[0],scatterPlots[1],c=MW[scatterPlots[2]]["01-Jul-20"]["12:00:00 AM"], cmap='gray')
+        
+#     fig, ax = plt.subplots()
+#     for i in range(0,len(scatterPlots[0])):
+# #        print("aaaa:  " + str(scatterPlots[0][i]) + " - " + str(scatterPlots[1][i]) + " - " + str(scatterPlots[2][i]))
+#         ax.scatter(scatterPlots[0][i],scatterPlots[1][i],s=MW[scatterPlots[2][i]]["01-Jul-20"]["12:00:00 AM"])
+#     plt.show()
+#     plt.scatter(scatterPlots[0],scatterPlots[1],c=MW[scatterPlots[2]]["01-Jul-20"]["12:00:00 AM"], cmap='gray')
     
 if __name__ == "__main__":
     if subsetData:
-#        if SelectedData == 0:
-#            loadMWData(".\Data\subset")
-#        elif SelectedData == 1:    
-#            loadMVAData(".\Data\subset")
-#        else:
-#            loadAllData(".\Data\subset")
-#    else:
-#        if SelectedData == 0:
+        if SelectedData == 0:
+            loadMWData(".\Data\subset")
+        elif SelectedData == 1:    
+            loadMVAData(".\Data\subset")
+        else:
+            loadAllData(".\Data\subset")
+    else:
+        if SelectedData == 0:
             loadMWData(".\Data\Energex-Network-Substation-Load-Data-2020-21")
-#        elif SelectedData == 1:    
-#            loadMVAData(".\Data\Energex-Network-Substation-Load-Data-2020-21")
-#        else:
-#            loadAllData(".\Data\Energex-Network-Substation-Load-Data-2020-21")
+        elif SelectedData == 1:    
+            loadMVAData(".\Data\Energex-Network-Substation-Load-Data-2020-21")
+        else:
+            loadAllData(".\Data\Energex-Network-Substation-Load-Data-2020-21")
             
-#    getLocations(".\Data")
-    
     LoadCoordsOntoMap(".\Data")
-    
-    
-    
-
-#print(MW)
